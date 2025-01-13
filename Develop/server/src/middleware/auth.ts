df4118f07933ed3842fe763ff5f246a25a3b9c4a
Kbasius <kbasius@gmail.com>
2025-01-12 22:24:59 -0500

    Added to auth-routes.ts
    completed the auth-routes todo

--- Develop/server/src/middleware/auth.ts
+++ Develop/server/src/middleware/auth.ts
@@ -7,13 +7,14 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
+  // Looks for the token in the authorization header.
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
-
+  // if no token send unauthorized error.
   if (!token) {
     return res.sendStatus(401);
   }
-
+  // this tries to verify the token if one is found.
   jwt.verify(token, process.env.JWT_SECRET as string, (err: jwt.VerifyErrors | null, user: JwtPayload | undefined) => {
     if (err) {
       return res.sendStatus(403);
--- Develop/server/src/routes/auth-routes.ts
+++ Develop/server/src/routes/auth-routes.ts
@@ -4,7 +4,32 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
-  // TODO: If the user exists and the password is correct, return a JWT token
+  // TODO: If the user exists and the password is correct, return a JWT token.
+  const { username, password } = req.body;
+
+  try {
+    // Looks for a user by their username
+    const user = await User.findOne({ where: { username } });
+    // Start of an if statement that checks if the user is real.
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid username or password' });
+    }
+    // if user is real, it then compares passwords for a match.
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    // error for miss matching passwords.
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid username or password' });
+    }
+    // if everything is still good, we create a token that lasts 1 hour.
+    const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET as string, {
+      expiresIn: '1h',
+    });
+
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' }); // Server error
+  }
 };
 
 const router = Router();
