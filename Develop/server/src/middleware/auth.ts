3eac3ec87c9fa2e5ad15ef22ff1a2456a25febbc
Kbasius <kbasius@gmail.com>
2025-01-21 18:57:22 -0500

    I run npm run build and fix the errors until it works
    Overload errors on (err: jwt.VerifyErrors | null, user: JwtPayload | undefined) => { so I got rid of it.
    
    Not all paths have a value error on (req: Request, res: Response, next: NextFunction) => { so we fixed that.

--- Develop/server/src/middleware/auth.ts
+++ Develop/server/src/middleware/auth.ts
@@ -1,11 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
-interface JwtPayload {
+interface JwtPayload extends jwt.JwtPayload {
   username: string;
 }
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+export const authenticateToken = (req: Request, res: Response, next: NextFunction): Response | void => {
   // TODO: verify the token exists and add the user data to the request object
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
@@ -14,12 +14,17 @@ export const authenticateToken = (req: Request, res: Response, next: NextFunctio
     return res.sendStatus(401);
   }
 
-  jwt.verify(token, process.env.JWT_SECRET as string, (err: jwt.VerifyErrors | null, user: JwtPayload | undefined) => {
+ 
+  jwt.verify(token, process.env.JWT_SECRET as string, (err, decoded) => {
+
     if (err) {
       return res.sendStatus(403);
     }
 
-    req.user = user;
-    next();
+    if (decoded) {
+      req.user = decoded as JwtPayload;
+      return next();
+    }
+    return res.sendStatus(403);
   });
 };
